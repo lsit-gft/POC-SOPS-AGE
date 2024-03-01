43f481e3b3582e759b67eacf47d990ae73655734
lsit-gft <lsit@gft.com>
2024-03-01 12:00:05 +0100

    test

---
+++ scripts/decrypt.sh
@@ -0,0 +1,10 @@
+echo -n '#!/bin/bash
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${scriptDir}/.." || exit 1
+
+export SOPS_AGE_KEY_FILE=$(pwd)/secrets/age-key.txt
+exec 3<<< "$(cat $1)"
+sops --decrypt --input-type json --output-type json /dev/fd/3
+' >> scripts/decrypt.sh
+chmod u+x scripts/decrypt.sh
